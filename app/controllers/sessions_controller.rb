db51153087b0d789ab9968b0e9897114d3a248a5
Kilmer L. Aleluia <kilmerluiz@globo.com>
2016-07-30 15:33:34 -0300

    Model tambem adicionado

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+	def new 
+		@session = Session.new
+	end
+
+	def index
+		@session = Session.new.all
+	end
+
+	def create
+		@session = Session.new params.require(:session).permit(:client)
+		@session.save
+		redirect_to new_session_path
+	end
 end
+
