16bd95fdd5e0fd7a2d8869cf9530b607767a8310
Kilmer L. Aleluia <kilmerluiz@globo.com>
2016-07-31 13:05:58 -0300

    inclusao teste

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,7 +7,9 @@ describe SessionsController do
 	end
 
 	it 'edit a session' do
-		
+		post :create, params:{session: {client: 'bilso, o próprio', data: '2016-07-31 15:12:04', imagem:'imagem', tamanho: '30'}}
+		post :create, params:{session: {client: 'bilseiro', data: '2016-07-31 15:12:04', imagem:'imagem', tamanho: '30'}, id: 1}
+		expect(Session.find(1)).to eq 'bilseiro'
 	end
 end
 
